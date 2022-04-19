c4e302f674c96cc501209d130bf7871425796c70
Iurii Zamiatin <notiurii@gmail.com>
2022-04-19 08:49:21 +0100

    A few misc fixes for profuzzbench

--- build.sh
+++ build.sh
@@ -18,8 +18,8 @@ BUILD_DIR=$(pwd)
 #### Build AFLNet ####
 cd "${BUILD_DIR}"
 
-BUILD_TARGETS=("afl-fuzz" "afl-fuzz-noaffin" "afl-fuzz-print" "afl-fuzz-noaffin-print" "afl-fuzz-noaffin-coredump" "afl-fuzz-long" "afl-fuzz-noaffin-long")
-BUILD_FLAGS=("" "-DNOAFFIN_BENCH=1" "-DPRINT_BENCH=1" "-DNOAFFIN_BENCH=1 -DPRINT_BENCH=1" "-DNOAFFIN_BENCH=1 -DCORE_BENCH=1 -DLONG_BENCH=1" "-DLONG_BENCH=1" "-DNOAFFIN_BENCH=1 -DLONG_BENCH=1")
+BUILD_TARGETS=("afl-fuzz-noaffin" "afl-fuzz-noaffin-long")
+BUILD_FLAGS=("-DNOAFFIN_BENCH=1" "-DNOAFFIN_BENCH=1 -DLONG_BENCH=1")
 
 git clone https://github.com/andronat/aflnet.git --branch mymaster --single-branch aflnet
 cd aflnet
@@ -37,6 +37,9 @@ for TARGET in "${BUILD_TARGETS[@]}"; do
   ((IT = IT + 1))
 done
 
+# Profuzzbench always uses binary called afl-fuzz
+ln -s ${FINISH_DIR}/afl-fuzz-noaffin-long ${FINISH_DIR}/afl-fuzz
+
 #### Build SnapFuzz ####
 cd "${BUILD_DIR}"
 
@@ -49,31 +52,5 @@ cmake -DCMAKE_BUILD_TYPE=RELEASE ..
 make -j
 cp ./sabre "${FINISH_DIR}/sabre"
 cp ./plugins/sbr-afl/libsbr-afl.so "${FINISH_DIR}/libsbr-afl.so"
-
-cmake -DCMAKE_BUILD_TYPE=RELEASE -DSF_MEMFS=OFF -DSF_STDIO=OFF -DSF_SLEEP=OFF -DSF_SMARTDEFER=OFF ..
-make -j
-cp ./sabre "${FINISH_DIR}/sabre"
-cp ./plugins/sbr-afl/libsbr-afl.so "${FINISH_DIR}/libsbr-afl-only-protocol.so"
-cp ./plugins/sbr-afl/libsbr-afl.so "${FINISH_DIR}/libsbr-afl-and-affin.so"
-
-cmake -DCMAKE_BUILD_TYPE=RELEASE -DSF_MEMFS=OFF -DSF_STDIO=OFF -DSF_SLEEP=ON -DSF_SMARTDEFER=OFF ..
-make -j
-cp ./sabre "${FINISH_DIR}/sabre"
-cp ./plugins/sbr-afl/libsbr-afl.so "${FINISH_DIR}/libsbr-afl-and-sleep.so"
-
-cmake -DCMAKE_BUILD_TYPE=RELEASE -DSF_MEMFS=OFF -DSF_STDIO=ON -DSF_SLEEP=ON -DSF_SMARTDEFER=OFF ..
-make -j
-cp ./sabre "${FINISH_DIR}/sabre"
-cp ./plugins/sbr-afl/libsbr-afl.so "${FINISH_DIR}/libsbr-afl-and-stdio.so"
-
-cmake -DCMAKE_BUILD_TYPE=RELEASE -DSF_MEMFS=OFF -DSF_STDIO=ON -DSF_SLEEP=ON -DSF_SMARTDEFER=ON ..
-make -j
-cp ./sabre "${FINISH_DIR}/sabre"
-cp ./plugins/sbr-afl/libsbr-afl.so "${FINISH_DIR}/libsbr-afl-and-defer.so"
-
-cmake -DCMAKE_BUILD_TYPE=RELEASE -DSF_MEMFS=ON -DSF_STDIO=ON -DSF_SLEEP=ON -DSF_SMARTDEFER=ON ..
-make -j
-cp ./sabre "${FINISH_DIR}/sabre"
-cp ./plugins/sbr-afl/libsbr-afl.so "${FINISH_DIR}/libsbr-afl-and-fs.so"
 
 cd $RETURN_PWD
